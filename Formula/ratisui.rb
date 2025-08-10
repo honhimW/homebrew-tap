e56bf9f1876d8e1b903e45c4f3e018e8b03ba011
honhimW <honhimw@outlook.com>
2025-08-11 00:34:54 +0800

    Update ratisui.rb

--- Formula/ratisui.rb
+++ Formula/ratisui.rb
@@ -1,10 +1,13 @@
 class Ratisui < Formula
   desc "Redis TUI build with Ratatui, written in Rust"
   homepage "https://github.com/honhimW/ratisui"
-  version "0.5.4"
+  # version "0.5.4"
   license all_of: ["MIT"]
 
   if OS.mac?
+    if Hardware::CPU.intel?
+      odie "Intel macOS is not supported"
+    end
     url "https://github.com/honhimW/ratisui/releases/download/0.5.4/ratisui-mac.tar.gz"
     sha256 "0a5aaa950eaa179a4ad58d5470a58d98387a7a14db00b296c5df9f2eeed07724"
   elsif OS.linux?
