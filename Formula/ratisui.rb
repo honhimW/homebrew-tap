dead149c3745e7815f3f4bbcc78f523fb1bc86f3
honhimW <honhimw@outlook.com>
2025-08-11 00:20:21 +0800

    Update ratisui.rb

--- Formula/ratisui.rb
+++ Formula/ratisui.rb
@@ -1,15 +1,14 @@
 class Ratisui < Formula
-  desc "Redis TUI build with Ratatui, written in Rust."
+  desc "Redis TUI build with Ratatui, written in Rust"
   homepage "https://github.com/honhimW/ratisui"
   version "0.5.4"
   license all_of: ["MIT"]
 
-  on_macos do
+  if OS.mac?
     url "https://github.com/honhimW/ratisui/releases/download/0.5.4/ratisui-mac.tar.gz"
     sha256 "0a5aaa950eaa179a4ad58d5470a58d98387a7a14db00b296c5df9f2eeed07724"
   end
-
-  on_linux do
+  elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/honhimW/ratisui/releases/download/0.5.4/ratisui-linux-aarch64.tar.gz"
       sha256 "94ac64065170776f3d10bd29b857c420dd90c514d56f422e555034ed9feb42d7"
@@ -23,13 +22,7 @@ class Ratisui < Formula
     bin.install "ratisui"
   end
 
-  
-  def install
-    bin.install "ratisui"
-  end
-  
   test do
     system "#{bin}/ratisui --version"
   end
-  
 end
